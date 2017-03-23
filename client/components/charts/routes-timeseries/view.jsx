c6fd926c197bd82312c2422b0b0e8b2e1f4cbf01
Matthias Hannig <matthias@hannig.cc>
2017-03-23 17:30:17 +0100

    added test chartjs

--- client/components/charts/routes-timeseries/view.jsx
+++ client/components/charts/routes-timeseries/view.jsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import {connect} from 'react-redux'
 
+import Chart from 'chart.js'
 
 import {loadRoutesTimeseries} from './actions'
 
@@ -32,10 +33,54 @@ class TimeseriesView extends React.Component {
 		}
 	}
 
+	drawChart() {
+		var myChart = new Chart(this.ctx, {
+				type: 'bar',
+				data: {
+						labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+						datasets: [{
+								label: '# of Votes',
+								data: [12, 19, 3, 5, 2, 3],
+								backgroundColor: [
+										'rgba(255, 99, 132, 0.2)',
+										'rgba(54, 162, 235, 0.2)',
+										'rgba(255, 206, 86, 0.2)',
+										'rgba(75, 192, 192, 0.2)',
+										'rgba(153, 102, 255, 0.2)',
+										'rgba(255, 159, 64, 0.2)'
+								],
+								borderColor: [
+										'rgba(255,99,132,1)',
+										'rgba(54, 162, 235, 1)',
+										'rgba(255, 206, 86, 1)',
+										'rgba(75, 192, 192, 1)',
+										'rgba(153, 102, 255, 1)',
+										'rgba(255, 159, 64, 1)'
+								],
+								borderWidth: 1
+						}]
+				},
+				options: {
+						scales: {
+								yAxes: [{
+										ticks: {
+												beginAtZero:true
+										}
+								}]
+						}
+				}
+		});
+	}
+
 	render() {
+		if (this.ctx) {
+			this.drawChart();
+		}
+
 		return(
-			<div>
+			<div className="routes-timeseries-chart">
 				Timeseries View... chart...
+				<canvas className="chart-view" ref={(el) => this.ctx = el}></canvas>
 			</div>
 		);
 	}
@@ -45,7 +90,6 @@ class TimeseriesView extends React.Component {
 
 export default connect(
 	(state, ownProps) => {
-		console.log(ownProps);
 		let key = `${ownProps.rsId}_${ownProps.asn}_${ownProps.neighbourAddress}`;
 		return {
 			timeseries: state.charts.routes.series[key],
